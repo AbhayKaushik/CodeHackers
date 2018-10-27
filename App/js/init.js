e6325042001081325e1a0c6724c7e5fdf5c0e48a
Siddhantj929 <siddhantj929@gmail.com>
2018-10-27 11:12:33 +0530

    Added register and login pages

--- App/js/init.js
+++ App/js/init.js
@@ -4,5 +4,15 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $('header .brand .menu-icon').click(function() {
+      $('.menu-list').addClass('activate');
+      $('menu').addClass('activate');
+    })
+    
+    $('menu').click(function() {
+      $('.menu-list').removeClass('activate');
+      $('menu').removeClass('activate');
+    })
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
