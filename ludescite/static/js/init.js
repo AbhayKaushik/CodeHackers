21db091aad15ea4d1f6c440f505248e3416b252c
Divyansh Agarwal <divag.bvp@gmail.com>
2018-10-27 16:09:04 +0530

    Added templates

--- ludescite/static/js/init.js
+++ ludescite/static/js/init.js
@@ -1,18 +1,35 @@
-(function($){
-  $(function(){
+(function ($) {
+	$(function () {
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+		$('.sidenav').sidenav();
+		$('.parallax').parallax();
 
-    $('header .brand .menu-icon').click(function() {
-      $('.menu-list').addClass('activate');
-      $('menu').addClass('activate');
-    })
-    
-    $('menu').click(function() {
-      $('.menu-list').removeClass('activate');
-      $('menu').removeClass('activate');
-    })
+		// Nav menu icon click event
+		$('header .brand .menu-icon').click(function () {
+			$('.menu-list').addClass('activate');
+			$('menu').addClass('activate');
+		})
 
-  }); // end of document ready
+		// Nav menu list ending event
+		$('menu').click(function (event) {
+			// event.preventDefault();
+			$('.menu-list').removeClass('activate');
+			$('menu').removeClass('activate');
+		})
+
+		// page-transitions
+		$(".board-wrapper").css("display", "none");
+		$(".board-wrapper").fadeIn(1000);
+
+		$(".transition").click(function (event) {
+			event.preventDefault();
+			linkLocation = this.href;
+			$(".board-wrapper").fadeOut(500, redirectPage);
+		});
+
+		function redirectPage() {
+			window.location = linkLocation;
+		}
+
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
